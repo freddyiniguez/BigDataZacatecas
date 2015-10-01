4f5c0ec83cd38e94189f2b7a8a3c13fc7399354a
Freddy Íñiguez López <iniguez.devt@gmail.com>
2015-10-01 08:15:21 -0500

    Script to real sample modified. Some issues were fixed.

--- real_samples/telefonia_movil_mexico.R
+++ real_samples/telefonia_movil_mexico.R
@@ -9,10 +9,10 @@ telefonia <- read.csv("suscriptores_telefonia_movil.csv")
 
 ### Suscriptores de telefonia movil por cada 100 habitantes del primer semestre ###
 suscriptores <- telefonia$suscriptores.por.cada.100.semestre.1
-x1 <- barplot(suscriptores, main = "Suscriptores de telefonia movil en Mexico", names.arg = telefonia$anios, cex.names = 0.8, xlab="Periodo 1995 - 2014", ylab = "Numero de suscriptores por cada 100 habitantes, 1er Sem.", beside = TRUE, ylim = c(0,100), col = "#3D56B2")
+x1 <- barplot(suscriptores, main = "Suscriptores de telefonia movil en Mexico", names.arg = telefonia$anios, cex.names = 0.7, xlab="Periodo 1995 - 2014", ylab = "Numero de suscriptores por cada 100 habitantes, 1er Sem.", beside = TRUE, ylim = c(0,100), col = "#3D56B2")
 text(x1, suscriptores, labels = suscriptores, pos = 3, offset = .5, col = "black")
 
 ### Suscriptores de telefonia movil del primer semestre ###
 suscriptores_total <- telefonia$suscriptores.total.semestre.1
-x2 <- barplot(suscriptores_total, main="Suscriptores de telefonia movil en Mexico", names.arg = telefonia$anios, cex.names = 0.8,  xlab = "Periodo 1995 - 2014", ylab = "Numero de suscriptores en total, 1er Sem.", beside = TRUE, ylim = c(0,110000000), col = "#3D56B2")
+x2 <- barplot(suscriptores_total, main="Suscriptores de telefonia movil en Mexico", names.arg = telefonia$anios, cex.names = 0.7,  xlab = "Periodo 1995 - 2014", ylab = "Numero de suscriptores en total, 1er Sem.", beside = TRUE, ylim = c(0,110000000), col = "#3D56B2")
 text(x2, suscriptores_total, labels = suscriptores_total, pos = 3, offset = .5, cex = 0.6, col = "black")
