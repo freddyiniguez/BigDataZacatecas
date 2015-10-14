0568cd965c9e84b75e5d5dddafc4532d26437821
Freddy Íñiguez López <iniguez.devt@gmail.com>
2015-10-14 08:47:47 -0500

    Script 3.2 added.

---
+++ 03.2 Experiments vs Observational Studies.R
@@ -0,0 +1,16 @@
+# * In an experiment, the investigator manipulates in some way the environment she or he is studying.
+# * There are so many concepts about experiments to explain, so, to do not just copy and paste it here, it is important to hit the section 3.2.
+
+# * Observational Studies are use to determine the behaviour or relationships between variables. This means that if we want to know the relationship
+# * between two or more variables, we can simple use an observational study.
+# * Obviously, you don't need to manipulate the environment.
+# * We can find two kinds of observational studies:
+#     - Prospective: this means that the participants are tracked or followed in the future.
+#     - Retrospective: this means that you collected data taking a look back in the past.
+
+# * To know the difference between an experiment and an observational study we can simple ask these few questions:
+# * (If you get a 'No' in one of these questions, the experiment cannot be performed.)
+#     - Is there a control group?
+#     - Is there (at least) one treatment group?
+#     - Did the researcher randomly assign subjects to these groups?
+#     - Did the researcher manipulate the environment in some way?
