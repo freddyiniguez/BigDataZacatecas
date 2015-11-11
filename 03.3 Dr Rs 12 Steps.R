5e399818e38c655ef046dd2988ac9380ac8d60f1
Freddy Íñiguez López <iniguez.devt@gmail.com>
2015-11-11 07:16:37 -0600

    Script 3.3 added.

---
+++ 03.3 Dr Rs 12 Steps.R
@@ -0,0 +1,14 @@
+# These are an abbreviated steps from Dr. Mike Hayden (http://technology.indstate.edu/directory/haydenm.htm)' 16 steps approach:
+
+# 1) State the overall problem and research question(s).
+# 2) State technique/methodology that will be used to dra a conclusion for this research question.
+# 3) Describe your population, sample, and sampling procedure.
+# 4) Data collection.
+# 5) Explain variables used.
+# 6) State assumptions and limitations.
+# 7) State the null hypothesis and allternative hypothesis.
+# 8) Set and explain level of significance (alpha).
+# 9) Include descriptive statistics + chats, graphs, or plots that describe your data set. 
+# 10) Calculate the test statistic (T.S.).
+# 11) Draw a picture, then find the p-value.
+# 12) Draw conclusion, provide interpretation, and confirm with confidence interval: is p-value z a(alpha)) If so, reject the null.
